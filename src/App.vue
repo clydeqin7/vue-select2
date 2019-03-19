0ae9ea7c79a8866d7b87f85f3a15c38c28e3b483
Aqin7 <clydeqin7@foxmail.com>
2019-03-19 18:01:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>{{ msg }}</div>
+    <vueSelect2 :options="options" label="label" value="value" v-model="text"></vueSelect2>
+  </div>
+</template>
+
+<script>
+import vueSelect2 from './components/vueSelect2'
+
+export default {
+  name: 'App',
+  components: { vueSelect2 },
+  data () {
+    return {
+      text: '',
+      msg: '一个Vue中使用的selecet2组件',
+      options: [
+        { name: '苹果', value: 1, label: 'label1' },
+        { name: '香蕉', value: 2, label: 'label2' },
+        { name: '西瓜', value: 3, label: 'label3' },
+        { name: '芒果', value: 4, label: 'label4' }
+      ]
+    }
+  },
+  mounted() {
+    // setTimeout(() => {
+    //   this.options = [
+    //     { name: '苹果1', value: 11, label: 'label5' },
+    //     { name: '香蕉1', value: 12, label: 'label6' },
+    //     { name: '西瓜1', value: 13, label: 'label7' },
+    //     { name: '芒果1', value: 14, label: 'label8' }
+    //   ]
+    // }, 10000)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/vueSelect2.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="select1">
+    <select id="vueSelect2" @change="selectChange">
+      <option v-for="item in options" :value="item[value !== '' ? value : label]">{{item[label]}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+    $(function(){
+      $('#vueSelect2').select2({
+        language: 'zh-CN'
+      });
+    });
+    export default {
+      name: "vueSelect2",
+      model: {
+        prop: 'optionValue',
+        event: 'change'
+      },
+      props: {
+        optionValue: String,
+        options: {
+          type: Array,
+          default: function() {
+            return []
+          },
+          required: true,
+        },
+        label: [String, Number],
+        value: {
+          type: [String, Number],
+          default: ''
+        }
+
+      },
+      data() {
+        return {
+          text: ''
+        }
+      },
+      mounted() {
+        // $('#vueSelect2').on('change', function() {
+        //   console.log($('#vueSelect2').children('option:selected').val(), '--------')
+        // })
+      },
+      methods: {
+        selectChange() {
+          this.$emit('change', $event.target.optionValue)
+        }
+      }
+    }
+</script>
+
+<style scoped>
+.select1{
+  width: 200px;
+}
+#vueSelect2{
+  width: 200px;
+}
+</style>
