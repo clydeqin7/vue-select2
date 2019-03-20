a8274a23ec97ee132bd6a259379dd3ad43df3fcb
Aqin7 <clydeqin7@foxmail.com>
2019-03-20 17:33:16 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img src="./assets/logo.png">
     <div>{{ msg }}</div>
-    <vueSelect2 :options="options" label="label" value="value" v-model="text"></vueSelect2>
+    <vueSelect2 :options="options" label="label" tagValue="value" v-model="text1" @change="selectChange"></vueSelect2>
   </div>
 </template>
 
@@ -14,25 +14,37 @@ export default {
   components: { vueSelect2 },
   data () {
     return {
-      text: '',
+      text1: '',
       msg: '一个Vue中使用的selecet2组件',
       options: [
-        { name: '苹果', value: 1, label: 'label1' },
-        { name: '香蕉', value: 2, label: 'label2' },
-        { name: '西瓜', value: 3, label: 'label3' },
-        { name: '芒果', value: 4, label: 'label4' }
+        // { name: '苹果', value: 1, label: 'label1' },
+        // { name: '香蕉', value: 2, label: 'label2' },
+        // { name: '西瓜', value: 3, label: 'label3' },
+        // { name: '芒果', value: 4, label: 'label4' }
       ]
     }
   },
+  watch: {
+    text1: function(newVal, oldVal) {
+      console.log(newVal, 'newValue')
+      console.log(this.text1, 'text1')
+    }
+  },
   mounted() {
-    // setTimeout(() => {
-    //   this.options = [
-    //     { name: '苹果1', value: 11, label: 'label5' },
-    //     { name: '香蕉1', value: 12, label: 'label6' },
-    //     { name: '西瓜1', value: 13, label: 'label7' },
-    //     { name: '芒果1', value: 14, label: 'label8' }
-    //   ]
-    // }, 10000)
+    setTimeout(() => {
+      this.options = [
+        { name: '苹果1', value: 11, label: 'label5' },
+        { name: '香蕉1', value: 12, label: 'label6' },
+        { name: '西瓜1', value: 13, label: 'label7' },
+        { name: '芒果1', value: 14, label: 'label8' }
+      ]
+    }, 5000)
+  },
+  methods: {
+    selectChange(val) {
+      console.log('父亲的change', val)
+      // this.optionValue = ''
+    }
   }
 }
 </script>
--- src/components/vueSelect2.vue
+++ src/components/vueSelect2.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="select1">
-    <select id="vueSelect2" @change="selectChange">
-      <option v-for="item in options" :value="item[value !== '' ? value : label]">{{item[label]}}</option>
+  <div class="select1" id="vueSelect2Wrapper">
+    <select id="vueSelect2">
+      <option v-for="item in options" :value="item[tagValue !== '' ? tagValue : label]">{{item[label]}}</option>
     </select>
   </div>
 </template>
 
 <script>
-    $(function(){
-      $('#vueSelect2').select2({
-        language: 'zh-CN'
-      });
-    });
+
     export default {
       name: "vueSelect2",
       model: {
@@ -19,7 +15,7 @@
         event: 'change'
       },
       props: {
-        optionValue: String,
+        optionValue: [String, Number],
         options: {
           type: Array,
           default: function() {
@@ -28,35 +24,72 @@
           required: true,
         },
         label: [String, Number],
-        value: {
+        tagValue: {
           type: [String, Number],
           default: ''
-        }
-
+        },
       },
       data() {
         return {
           text: ''
         }
       },
+      watch: {
+        optionValue: function (newValue) {
+          console.log('=select2=', newValue)
+        }
+      },
+      created() {
+        $(function(){
+          $('#vueSelect2').select2({
+            placeholder: '请选择桥梁',
+            language: 'zh-CN'
+          });
+        });
+      },
       mounted() {
-        // $('#vueSelect2').on('change', function() {
-        //   console.log($('#vueSelect2').children('option:selected').val(), '--------')
-        // })
+        let _this = this
+        $('#vueSelect2').on('change', function() {
+          console.log($('#vueSelect2').children('option:selected').val(), '--------')
+          _this.$emit('change', $('#vueSelect2').children('option:selected').val())
+        })
       },
       methods: {
-        selectChange() {
-          this.$emit('change', $event.target.optionValue)
-        }
+        // selectChange() {
+        //   console.log($('#vueSelect2').children('option:selected').val(), '--------')
+        //   this.$emit('change', $event.target.optionValue)
+        //   // this.$emit('change', $('#vueSelect2').children('option:selected').val())
+        // }
       }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .select1{
   width: 200px;
 }
 #vueSelect2{
   width: 200px;
 }
+
+</style>
+<style lang="scss">
+  #vueSelect2Wrapper {
+    .select2-container--default .select2-selection--single .select2-selection__rendered {
+      color: #606266;
+    }
+    .select2-results{
+      ul.select2-results__options{
+        li.select2-results__option{
+          color: red !important;
+        }
+      }
+    }
+    :focus{
+      outline-color: red !important;
+    }
+    .select2-container--default .select2-search--dropdown .select2-search__field:focus {
+      outline-color: red !important;
+    }
+  }
 </style>
